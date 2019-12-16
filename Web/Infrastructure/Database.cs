b459d9ea90b99085ba662d7d95b4ab13daab4946
Darren Douglas <ddouglas@verdasys.com>
2019-12-16 16:40:43 -0500

    Refactor changes for data handling

--- Web/Infrastructure/Database.cs
+++ Web/Infrastructure/Database.cs
@@ -1,40 +1,79 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Data;
 
 namespace Web.Infrastructure
 {
     using System.Data.Common;
     using System.Data.SqlClient;
+    using System.Collections.Generic;
 
     public class Database
     {
+        // Replace this with a connection pooling system to reduce the opens and number of concurrent connections to 
+        // a managed limit.
+
         private readonly SqlConnection _connection;
 
-        public Database()
+        public Database(String ConnectionString)
         {
-            _connection = new SqlConnection("Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=BrainWAre;Integrated Security=SSPI;AttachDBFilename=D:\\BrainWare\\Web\\App_Data\\BrainWare.mdf");
+            _connection = new SqlConnection(ConnectionString);
+        }
 
+        ~Database()
+        {
+            // Make sure the connection is closed in case the uninit method is not called
+            uninit();
+        }
+
+        /// <summary>
+        /// Initialize the database connection.
+        /// </summary>
+        public void init()
+        {
             _connection.Open();
         }
 
+        /// <summary>
+        /// Close the database connection.
+        /// </summary>
+        public void uninit()
+        {
+            // Close the connection to the database
+            if (_connection.State == ConnectionState.Open)
+            {
+                _connection.Close();
+            }
+        }
 
+        /// <summary>
+        /// Return the execute reader for the query
+        /// </summary>
+        /// <param name="query"></param>
+        /// <returns>The execute reader for the command</returns>
         public DbDataReader ExecuteReader(string query)
         {
-           
-
             var sqlQuery = new SqlCommand(query, _connection);
 
             return sqlQuery.ExecuteReader();
         }
 
-        public int ExecuteNonQuery(string query)
+
+        public DbDataReader ExecuteReader(string query, Dictionary<string, string> args)
         {
             var sqlQuery = new SqlCommand(query, _connection);
+            if(args != null && args.Count > 0)
+            {
+                foreach(var arg in args.Keys)
+                {
+                    sqlQuery.Parameters.AddWithValue(arg, args[arg]);
+                }
+            }
+            else
+            {
+                throw new Exception("Bad Args for Sql Query Suppiled.");
+            }
 
-            return sqlQuery.ExecuteNonQuery();
+            return sqlQuery.ExecuteReader();
         }
-
     }
 }
--- Web/Infrastructure/OrderService.cs
+++ Web/Infrastructure/OrderService.cs
@@ -1,85 +1,178 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
 namespace Web.Infrastructure
 {
     using System.Data;
     using Models;
+    using System.Data.SqlClient;
 
     public class OrderService
     {
+        private const string DB_CONNECTION_STRING = "Data Source=(LocalDb)\\MSSQLLocalDB;Initial Catalog=BrainWAre;Integrated Security=SSPI;AttachDBFilename=e:\\BrainWare\\Web\\App_Data\\BrainWare.mdf";
+
         public List<Order> GetOrdersForCompany(int CompanyId)
         {
-
-            var database = new Database();
-
-            // Get the orders
-            var sql1 =
-                "SELECT c.name, o.description, o.order_id FROM company c INNER JOIN [order] o on c.company_id=o.company_id";
-
-            var reader1 = database.ExecuteReader(sql1);
+            // These should be defined centraly to match the schema names for the columns
+            const string COLUMN_ORDER_NAME = "name";
+            const string COLUMN_ORDER_DESCRIPTION = "description";
+            const string COLUMN_ORDER_ID = "order_id";
 
             var values = new List<Order>();
-            
-            while (reader1.Read())
+            Database connection = new Database(DB_CONNECTION_STRING);
+
+            try
             {
-                var record1 = (IDataRecord) reader1;
+                // Open the connection to the database.
+                connection.init();
 
-                values.Add(new Order()
+                // Get the orders
+                var sql = "SELECT c.name, o.description, o.order_id FROM company c INNER JOIN [order] o on c.company_id=o.company_id";
+                SqlDataReader reader = (SqlDataReader)connection.ExecuteReader(sql);
+
+                try
                 {
-                    CompanyName = record1.GetString(0),
-                    Description = record1.GetString(1),
-                    OrderId = record1.GetInt32(2),
-                    OrderProducts = new List<OrderProduct>()
-                });
-
-            }
-
-            reader1.Close();
-
-            //Get the order products
-            var sql2 =
-                "SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price FROM orderproduct op INNER JOIN product p on op.product_id=p.product_id";
-
-            var reader2 = database.ExecuteReader(sql2);
-
-            var values2 = new List<OrderProduct>();
-
-            while (reader2.Read())
-            {
-                var record2 = (IDataRecord)reader2;
-
-                values2.Add(new OrderProduct()
-                {
-                    OrderId = record2.GetInt32(1),
-                    ProductId = record2.GetInt32(2),
-                    Price = record2.GetDecimal(0),
-                    Quantity = record2.GetInt32(3),
-                    Product = new Product()
+                    // If we have results process them into an list of objects
+                    if (reader != null && reader.HasRows)
                     {
-                        Name = record2.GetString(4),
-                        Price = record2.GetDecimal(5)
+                        while (reader.Read())
+                        {
+                            var order = new Order
+                            {
+                                CompanyName = reader[COLUMN_ORDER_NAME].ToString(),
+                                Description = reader[COLUMN_ORDER_DESCRIPTION].ToString(),
+                                OrderId = (int)reader[COLUMN_ORDER_ID],
+                                OrderProducts = new List<OrderProduct>()
+                            };
+
+                            // Load the order product details.
+                            order.OrderProducts = getOrderDetails(order.OrderId);
+
+                            // Calculate the total of the order
+                            order.OrderTotal = getOrderTotal(order.OrderProducts);
+
+                            values.Add(order);
+                        }
                     }
-                });
-             }
 
-            reader2.Close();
-
-            foreach (var order in values)
-            {
-                foreach (var orderproduct in values2)
-                {
-                    if (orderproduct.OrderId != order.OrderId)
-                        continue;
-
-                    order.OrderProducts.Add(orderproduct);
-                    order.OrderTotal = order.OrderTotal + (orderproduct.Price * orderproduct.Quantity);
                 }
+                finally
+                {
+                    // Close the reader
+                    if (!reader.IsClosed)
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Normally Log an error 
+                Console.Write("Exception caught - " + ex.Message);
+            }
+            finally
+            {
+                // Close the connection to the database
+                connection.uninit();
             }
 
             return values;
+        }
+
+        /// <summary>
+        /// Return the order details for the order id specified
+        /// </summary>
+        /// <param name="OrderId"></param>
+        /// <returns>List of OrderProduct objects</returns>
+        private List<OrderProduct> getOrderDetails(Int32 OrderId)
+        {
+            // These should be defined centraly to match the schema names for the columns
+            const string COLUMN_ORDER_ID = "order_id";
+            const string COLUMN_PRODUCT_ID = "product_id";
+            const string COLUMN_QUANTITY = "quantity";
+            const string COLUMN_PRODUCT_NAME = "name";
+            const string COLUMN_PRODUCT_PRICE = "price";
+
+            var orderProducts = new List<OrderProduct>();
+
+            Database connection = new Database(DB_CONNECTION_STRING);
+            try
+            {
+                // Open the connection to the database
+                connection.init();
+
+                // Create the SQL statement for the requested order id
+                var args = new Dictionary<string, string>
+                {
+                    { COLUMN_ORDER_ID, OrderId.ToString()}
+                };
+
+                var sql =
+                    "SELECT op.price, op.order_id, op.product_id, op.quantity, p.name, p.price FROM orderproduct op INNER JOIN product p on op.product_id=p.product_id WHERE op.order_id = @" + COLUMN_ORDER_ID;
+                SqlDataReader reader = (SqlDataReader)connection.ExecuteReader(sql, args);
+
+                try
+                {
+                    // If there are results load them into a list of order product objects
+                    if (reader != null && reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var orderProduct = new OrderProduct
+                            {
+                                OrderId = (int)reader[COLUMN_ORDER_ID],
+                                ProductId = (int)reader[COLUMN_PRODUCT_ID],
+                                Quantity = (int)reader[COLUMN_QUANTITY],
+                                Product = new Product()
+                                {
+                                    Name = reader[COLUMN_PRODUCT_NAME].ToString(),
+                                    Price = (Decimal)reader[COLUMN_PRODUCT_PRICE]
+                                }
+                            };
+
+                            orderProducts.Add(orderProduct);
+                        }
+                    }
+                }
+                finally
+                {
+                    // Close the reader
+                    if (!reader.IsClosed)
+                    {
+                        reader.Close();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Normally Log an error 
+                Console.Write("Exception caught - " + ex.Message);
+            }
+            finally
+            {
+                // Close the connection the database
+                connection.uninit();
+            }
+
+            return orderProducts;
+        }
+
+        /// <summary>
+        /// Return the total of the order.
+        /// </summary>
+        /// <param name="orderProducts"></param>
+        /// <returns>The total of the items in the order</returns>
+        private Decimal getOrderTotal(List<OrderProduct> orderProducts)
+        {
+            Decimal OrderTotal = 0;
+
+            // Total up all the items in the order
+            foreach (var orderproduct in orderProducts)
+            {
+                OrderTotal += (orderproduct.Product.Price * orderproduct.Quantity);
+            }
+
+            return OrderTotal;
         }
     }
 }
